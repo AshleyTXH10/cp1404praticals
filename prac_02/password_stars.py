741b1e2ceaa5ef73db94449fbf78fc6ab03b3bd2
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-09-23 16:23:53 +0800

    Add dochub to password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,16 +1,16 @@
 MINIMUM_PASSWORD_LENGTH = 10
 
 def main():
+    "simple password program"
     password = get_password()
-
     print_asteriks(password)
 
-
 def print_asteriks(password):
+    "Print amount of asteriks of password length"
     print("*" * len(password))
 
-
 def get_password():
+    "Get user password"
     password = input("Enter password: ")
     while len(password) < MINIMUM_PASSWORD_LENGTH:
         print("Password does not meet minimum password length!")
