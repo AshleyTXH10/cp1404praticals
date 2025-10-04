31379a4e449ccdc14fc607c075269db30159cb1f
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-05 00:28:11 +0800

    Updated files.py with question 2 code and fixed mistakes in question 1

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,6 +1,13 @@
 # Question 1
 FILENAME = "name.txt"
 name = input("What is your name? ")
-in_file = open(FILENAME, "w")
-print(name, file=in_file)
+out_file = open(FILENAME, "w")
+print(name, file=out_file)
+out_file.close()
+
+#Question 2
+FILENAME = "name.txt"
+in_file = open(FILENAME)
+name = in_file.read().strip()
 in_file.close()
+print(f"Hi {name}!")
