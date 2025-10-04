f4818ee73e70b19814daf6d58d0a74dae33cc4c2
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-04 23:39:35 +0800

    Update program to write output to a file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,6 +9,7 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
+FILENAME = "simulator_result.txt"
 MAX_INCREASE = 0.175  # 10%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
@@ -17,7 +18,9 @@ INITIAL_PRICE = 10.0
 
 number_of_days = 0
 price = INITIAL_PRICE
+out_file = open(FILENAME, 'w')
 print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -35,3 +38,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
 
     price *= (1 + price_change)
     print(f"On day {number_of_days} price is : ${price:,.2f}")
+    print(f"Day: {number_of_days}: ${price:,.2f}", file=out_file)
+out_file.close()
