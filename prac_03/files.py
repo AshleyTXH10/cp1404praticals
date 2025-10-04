b9b46df8e0bac0f08d2001919122173fc754a9e6
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-05 00:41:01 +0800

    Updated files.py with question 4 code

--- prac_03/files.py
+++ prac_03/files.py
@@ -11,3 +11,21 @@ in_file = open(FILENAME)
 name = in_file.read().strip()
 in_file.close()
 print(f"Hi {name}!")
+
+#Question 3
+FILENAME = "numbers.txt"
+with open("numbers.txt", "r") as in_file:
+    lines = in_file.readlines()
+
+first_number = int(lines[0])
+second_number = int(lines[1])
+print(f"{first_number + second_number}")
+
+#Question 4
+FILENAME = "numbers.txt"
+total = 0
+with open("numbers.txt") as in_file:
+    for line in in_file:
+        total += int(line.strip())
+
+print(f"{total }")
