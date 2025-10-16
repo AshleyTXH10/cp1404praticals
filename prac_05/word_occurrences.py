4c386bde64c5d5b9b8559ddc7fbc03bfdd152a3e
ashleytan <xinhuiashley.tan@my.jcu.edu.au>
2025-10-16 17:47:52 +0800

    Add and complete word occurrences exercise and file

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,21 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual: 15 minutes
+"""
+text = input("Text: ")
+words = sorted(text.split())
+
+word_count = {}
+# Counts the amount of times a word appears
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+word_width = max((len(word) for word in word_count))
+
+# Print word and the count in a formatted way
+for word, count in word_count.items():
+    print(f"{word:{word_width}} : {count}")
