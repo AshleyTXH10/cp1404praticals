232f6c04b5c0d419f519d9a1e514755d6e45bb16
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-30 11:06:19 +0800

    Define __init__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,3 +3,10 @@ Programming language
 Estimate: 20 minutes
 Actual:
 """
+
+class ProgrammingLanguage():
+    def __init__(self, programming_language="", typing="", reflection="", year=""):
+        self.programming_language = programming_language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
