a98498dd31ed7c237cf2d58bf3676078efa0312b
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-30 11:40:06 +0800

    Add loop to print out all programming languages that have dynamic typing

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,6 +1,16 @@
 from prac_06.programming_language import ProgrammingLanguage
 
+programming_languages = []
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+programming_languages.append(python)
+programming_languages.append(ruby)
+programming_languages.append(visual_basic)
+
+print("The dynamically typed languages are:")
+for program in programming_languages:
+    if program.is_dynamic():
+        print(f"{program.programming_language}")
+
 print(python)
