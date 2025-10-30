d98f80c488554ab3b845469f55e85453540aaef0
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-30 11:07:16 +0800

    Define is_dynamic() method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -10,3 +10,6 @@ class ProgrammingLanguage():
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
