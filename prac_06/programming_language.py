5b364c9f3dde238b98ff95fbd388e26786450e46
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-10-30 11:09:51 +0800

    Define __str__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,11 +5,14 @@ Actual:
 """
 
 class ProgrammingLanguage():
-    def __init__(self, programming_language="", typing="", reflection="", year=""):
+    def __init__(self, programming_language="", typing="", reflection=False, year=""):
         self.programming_language = programming_language
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        return f"{self.programming_language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
