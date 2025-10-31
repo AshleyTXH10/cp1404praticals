558de969a44a8a73eeda2afe60bfbbd67d306713
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-11-01 00:29:31 +0800

    Add guitar creating class loop

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,3 +2,11 @@ from prac_06.guitar import Guitar
 
 print("My guitars!")
 guitars = []
+name = input("Name: ").title()
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    print(f"{guitar} added.")
+    name = input("Name: ").title()
