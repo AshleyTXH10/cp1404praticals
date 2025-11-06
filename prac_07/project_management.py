b4b4610a467600c0e19cdc3866523f1e72951506
ashleytan <xinhuiashley.tan@my.jcu.edu.au>
2025-11-06 16:55:20 +0800

    Add all functions needed for running the program

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,3 +3,32 @@ Project Management Program
 estimate: 50 minutes
 actual:
 """
+def main():
+    """Runs the project management program"""
+    print("Main program")
+
+def load_projects():
+    """Prompt the user for a filename to load projects from and load them)"""
+    print("Load projects")
+
+def save_projects():
+    """Prompt the user for a filename to save projects to and save them"""
+    print("Save projects")
+
+def display_projects():
+    """Display two groups: incomplete projects; completed projects, both sorted by priority"""
+    print("Display")
+
+def filter_projects():
+    """Ask the user for a date and display only projects that start after that date, sorted by date"""
+    print("Filter projects by date")
+
+def add_new_project():
+    """Ask the user for the inputs and add a new project to memory"""
+    print("add")
+
+def update_project():
+    """Choose a project, then modify the completion % and/or priority - the user can leave either input blank to retain existing values"""
+    print("Update")
+
+main()
