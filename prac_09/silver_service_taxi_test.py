3291633edd46480caf39c07751b4e51fcfb91d82
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-11-17 20:56:41 +0800

    Add assertion test to silver service taxi test

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,9 +1,11 @@
 from silver_service_taxi import SilverServiceTaxi
+EXPECTED_FARE = 48.78
 
 def main():
     """Run tests for the SilverServiceTaxi class"""
     sst1 = SilverServiceTaxi(name="Toyota", fuel=100, fanciness=2)
     sst1.drive(18)
-    print(sst1.get_fare())
+    #print(sst1.get_fare())
+    assert sst1.get_fare() == EXPECTED_FARE, f"Fare expected: ${EXPECTED_FARE}, got: ${sst1.get_fare()}"
 
 main()
