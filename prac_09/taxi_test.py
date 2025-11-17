07b687ee9d247f41cac4676c8aedb48aa5fcae72
ashleytan <xinhuiashley.tan@my.jcu.edu.au>
2025-11-17 15:23:42 +0800

    Add class variable to taxi class and made modifications to taxi test file

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,7 +2,7 @@ from taxi import Taxi
 
 def main():
     """Run tests for the taxi class"""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(f"{my_taxi} ({my_taxi.get_fare()})")
     my_taxi.start_fare()
