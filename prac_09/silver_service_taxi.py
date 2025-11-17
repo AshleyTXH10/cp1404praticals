f09f33676ae4044b7e58989a7c6d30e4764dba91
ashleytan <xinhuiashley.tan@my.jcu.edu.au>
2025-11-17 17:05:59 +0800

    Create SilverServiceTaxi class with the requested modifications

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,21 @@
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+
+    def __init__(self, fanciness: float, **kwargs):
+        """Initialise a Silver Service Taxi instance, based on parent class Taxi."""
+        super().__init__(self, **kwargs)
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string like a Taxi but with flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.flagfall + self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare_distance = 0
