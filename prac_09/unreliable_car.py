e4931a8dddc62704b128028a6192fee77f718422
ashleytan <xinhuiashley.tan@my.jcu.edu.au>
2025-11-17 15:48:05 +0800

    Add modifications to UnreliableCar class and wrote tests to test the drive function

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,6 +1,7 @@
 from car import Car
 import random
 
+
 class UnreliableCar(Car):
     def __init__(self, reliability: float, **kwargs):
         super().__init__(**kwargs)
@@ -8,5 +9,12 @@ class UnreliableCar(Car):
 
     def drive(self, distance):
         if random.randint(1,100) < self.reliability:
-            self.fuel -= distance
-        self._odometer += distance
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self._odometer += distance
+            return distance
+        else:
+            return 0
