d2e512765dd4c359d782a2a24fb9a83e8a70eda8
Ashley <xinhuiashley.tan@my.jcu.edu.au>
2025-11-21 10:08:08 +0800

    Add docstring

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,11 +3,14 @@ import random
 
 
 class UnreliableCar(Car):
+    """Represent an unreliable Car object with a parent class of car."""
     def __init__(self, reliability: float, **kwargs):
+        """Initialise an unreliable car instance, based on parent class Car."""
         super().__init__(**kwargs)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Drive like parent Car but add in unreliability as well."""
         if random.randint(1,100) < self.reliability:
             if distance > self.fuel:
                 distance = self.fuel
